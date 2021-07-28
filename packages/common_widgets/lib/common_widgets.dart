29c9adde8657bcfd69f6985f1c4c0feb48a62eee
kume1a <kumela011@gmail.com>
2021-07-28 12:43:38 +0400

    added exports in common widgets module

---
+++ packages/common_widgets/lib/common_widgets.dart
@@ -0,0 +1,6 @@
+export 'src/bottom_nav/animated_bottom_nav.dart';
+export 'src/blank_container.dart';
+export 'src/dashed_rect.dart';
+export 'src/default_form.dart';
+export 'src/sliver_sized_box.dart';
+export 'src/tap_outside_to_clear_focus.dart';

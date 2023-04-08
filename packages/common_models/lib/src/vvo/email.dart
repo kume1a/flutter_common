e78c28a9fad203e7a39b4ddab39fe2184a0179bd
kume1a <kumela011@gmail.com>
2023-04-08 17:05:57 +0400

    change email regex

--- packages/common_models/lib/src/vvo/email.dart
+++ packages/common_models/lib/src/vvo/email.dart
@@ -21,7 +21,7 @@ class Email extends ValueObject<EmailFailure, String> {
 
   Email._(Either<EmailFailure, String> value) : super(value);
 
-  static final RegExp _patternEmail =
-      RegExp(r"^[a-zA-Z\d.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z\d]+\.[a-zA-Z]+");
+  static final RegExp _patternEmail = RegExp(
+      r'''^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$''');
   static final RegExp _patternWhitespace = RegExp(r'\s');
 }

a00dfa75989aa46b7601546a5f4d91edb3cbf673
kume1a <kumela011@gmail.com>
2023-05-29 17:27:15 +0400

    extend phone number validation

--- packages/common_models/lib/src/vvo/phone_number.dart
+++ packages/common_models/lib/src/vvo/phone_number.dart
@@ -1,20 +1,27 @@
+import 'package:libphonenumber_plugin/libphonenumber_plugin.dart';
+
 import '../core/either.dart';
 import '../failure/value_failure.dart';
 import 'core/value_object.dart';
 
 class PhoneNumber extends ValueObject<ValueFailure, String> {
-  factory PhoneNumber(String phoneNumber) {
-    if (phoneNumber.isEmpty) {
-      return PhoneNumber._(left(ValueFailure.empty));
-    } else if (!_patternPhoneNumber.hasMatch(phoneNumber.trim())) {
-      return PhoneNumber._(left(ValueFailure.invalid));
-    }
-    return PhoneNumber._(right(phoneNumber));
-  }
+  PhoneNumber._(Either<ValueFailure, String> value) : super(value);
 
   factory PhoneNumber.empty() => PhoneNumber._(left(ValueFailure.empty));
 
-  PhoneNumber._(Either<ValueFailure, String> value) : super(value);
+  static Future<PhoneNumber> create(String phoneNumber, String isoCode) async {
+    if (phoneNumber.isEmpty) {
+      return PhoneNumber._(left(ValueFailure.empty));
+    } else if (phoneNumber.length < 2) {
+      return PhoneNumber._(left(ValueFailure.invalid));
+    }
 
-  static final RegExp _patternPhoneNumber = RegExp(r'^(995)?\d{9}$');
+    final bool isValid = await PhoneNumberUtil.isValidPhoneNumber(phoneNumber, isoCode) ?? false;
+
+    if (!isValid) {
+      return PhoneNumber._(left(ValueFailure.invalid));
+    }
+
+    return PhoneNumber._(right(phoneNumber));
+  }
 }

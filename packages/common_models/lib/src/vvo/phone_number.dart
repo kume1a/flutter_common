ce3a0c392a6a378ebbc66f481de27e944b9b86ab
kume1a <kumela011@gmail.com>
2024-10-15 13:15:52 +0400

    remove libphonenumber dependency

--- packages/common_models/lib/src/vvo/phone_number.dart
+++ packages/common_models/lib/src/vvo/phone_number.dart
@@ -1,31 +1,31 @@
-import 'package:libphonenumber/libphonenumber.dart';
+// import 'package:libphonenumber/libphonenumber.dart';
 
-import '../core/either.dart';
-import '../error/value_error.dart';
-import 'core/value_object.dart';
+// import '../core/either.dart';
+// import '../error/value_error.dart';
+// import 'core/value_object.dart';
 
-class PhoneNumber extends ValueObject<ValueError, String> {
-  PhoneNumber._(super.value);
+// class PhoneNumber extends ValueObject<ValueError, String> {
+//   PhoneNumber._(super.value);
 
-  factory PhoneNumber.empty() => PhoneNumber._(left(ValueError.empty));
+//   factory PhoneNumber.empty() => PhoneNumber._(left(ValueError.empty));
 
-  static Future<PhoneNumber> create(String phoneNumber, String isoCode) async {
-    if (phoneNumber.isEmpty) {
-      return PhoneNumber._(left(ValueError.empty));
-    } else if (phoneNumber.length < 2) {
-      return PhoneNumber._(left(ValueError.invalid));
-    }
+//   static Future<PhoneNumber> create(String phoneNumber, String isoCode) async {
+//     if (phoneNumber.isEmpty) {
+//       return PhoneNumber._(left(ValueError.empty));
+//     } else if (phoneNumber.length < 2) {
+//       return PhoneNumber._(left(ValueError.invalid));
+//     }
 
-    final bool isValid = await PhoneNumberUtil.isValidPhoneNumber(
-          phoneNumber: phoneNumber,
-          isoCode: isoCode,
-        ) ??
-        false;
+//     final bool isValid = await PhoneNumberUtil.isValidPhoneNumber(
+//           phoneNumber: phoneNumber,
+//           isoCode: isoCode,
+//         ) ??
+//         false;
 
-    if (!isValid) {
-      return PhoneNumber._(left(ValueError.invalid));
-    }
+//     if (!isValid) {
+//       return PhoneNumber._(left(ValueError.invalid));
+//     }
 
-    return PhoneNumber._(right(phoneNumber));
-  }
-}
+//     return PhoneNumber._(right(phoneNumber));
+//   }
+// }
